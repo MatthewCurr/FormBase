fd0b2c8b91cc57eeb344afa1901ab43a1e658e99
Matthew <curran.wehttam@gmail.com>
2025-10-16 11:19:18 +1000

    Changed Tab Layout to Drawer Layout with File Based Routing.

--- app/tabs/(tabs)/_layout.jsx
+++ app/tabs/(tabs)/_layout.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import { Drawer } from 'expo-router/drawer';
+import { useNavigation } from '@react-navigation/native';
 import { Tabs } from 'expo-router';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+
+function DrawerIcon({ name, color }) {
+  return <FontAwesome size={18} style={{ marginRight: 8 }} name={name} color={color} />;
+}
 
 function TabBarIcon(props) {
   return <FontAwesome size={18} style={{ marginBottom: -3 }} {...props} />;
 }
 
-export default function TabLayout() {
+export default function DrawerLayout() {
+
   return (
-    <Tabs
-      screenOptions={{
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}
+    <Drawer
+      screenOptions={({ route }) => ({
+        headerShown:
+          route.name === 'Home' // Hide Header on Home Screen
+            ? useClientOnlyValue(false, false) 
+            : useClientOnlyValue(false, true),
+      })}
     >
-      <Tabs.Screen
-        name="tab1"
-        options={{
-          title: 'Tab 1',
-          tabBarIcon: ({ color }) => <TabBarIcon name="star-o" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="tab2"
-        options={{
-          title: 'Tab 4',
-          tabBarIcon: ({ color }) => <TabBarIcon name="star-o" color={color} />,
-        }}
-      />
-    </Tabs>
+    </Drawer>
   );
 }
+

9badc1236e121da26e7218ebcd4beced033e905e
Matthew <curran.wehttam@gmail.com>
2025-10-17 21:50:06 +1000

    Added About Page describing App.

--- app/tabs/(tabs)/About.jsx
+++ app/tabs/(tabs)/About.jsx
@@ -3,14 +3,54 @@ import { Center } from '@/components/ui/center';
 import { Divider } from '@/components/ui/divider';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
+import { Box } from '@/components/ui/box';
+import FileStack from '@/assets/icons/FileStack';
+import { useColorScheme } from '@/components/useColorScheme';
 
-export default function Tab2() {
+export default function About() {
+
+  const colorMode = useColorScheme();
+  const isDark = colorMode === 'dark';
+  
+  // Filestack Colours for Dark and Light Mode
+  const mainColor = isDark ? '#EBEAE9' : '#5B5551';
+  const secondaryColor = isDark ? '#272423' : '#C7C4BF';
+
   return (
-    <Center className="flex-1">
-      <Heading className="font-bold text-2xl">Expo- Tab 2</Heading>
+    <Box className="p-10 items-start">
+      {/* Icon and Title */}
+      <Box className="items-center p-4 -mt-6">
+        <Box className="">
+          <FileStack color={mainColor} secondaryColor={secondaryColor}/>
+        </Box>
+        <Heading className="text-xl">FormBase</Heading>
+      </Box>
+
+      {/* Description Text */}
+      <Text className=" text-lg">
+        The modern way to create custom forms. 
+        FormBase combines powerful features with an intuitive interface, 
+        letting you build professional forms in minutes.
+      </Text>
+
+      {/* Divider */}
       <Divider className="my-[30px] w-[80%]" />
-      <Text className="p-4">Example below to use gluestack-ui components.</Text>
-      <EditScreenInfo path="app/(app)/(tabs)/tab2.tsx" />
-    </Center>
+
+      {/* Section Heading */}
+      <Heading className="text-2xl mb-4">What we Offer</Heading>
+      
+      {/* Features List */}
+      <Box className="flex flex-col space-y-3">
+        <Text className="text-base text-left">
+          • <Text className="font-semibold">Dynamic Form Creation:</Text> Create forms that adapt to your needs.
+        </Text>
+        <Text className="text-base text-left">
+          • <Text className="font-semibold">In-Built Form Filter:</Text> Easily filter and manage form responses.
+        </Text>
+        <Text className="text-base text-left">
+          • <Text className="font-semibold">Map:</Text> Easily visualize location data.
+        </Text>
+      </Box>
+    </Box>
   );
 }

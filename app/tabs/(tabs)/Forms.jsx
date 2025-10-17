a694fcde0f2b140fc19a79d2421b392642fe67ba
Matthew <curran.wehttam@gmail.com>
2025-10-17 22:30:59 +1000

    Initial Form List Container and Add Button.

--- app/tabs/(tabs)/Forms.jsx
+++ app/tabs/(tabs)/Forms.jsx
@@ -5,37 +5,44 @@ import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
 import { Button, ButtonText } from '@/components/ui/button';
 import { useRouter } from 'expo-router';
+import { ScrollView, TouchableOpacity } from 'react-native';
+import { Box } from '@/components/ui/box';
+import PlusIcon from '@/assets/icons/Plus';
+import * as Haptics from 'expo-haptics';
 
-export default function Tab2() {
+export default function FormListScreen() {
   const router = useRouter();
 
-  return (
-    <Center className="flex-1">
-      <Heading className="font-bold text-2xl">Expo - Tab 1 Test</Heading>
-      <Divider className="my-[30px] w-[80%]" />
-      <Text className="p-4">Example below to use gluestack-ui components.</Text>
-      <EditScreenInfo path="app/(app)/(tabs)/tab1.tsx" />
-    
-      <Button
-        size="md"
-        className="bg-primary-500 px-6 py-2 rounded-full"
-        onPress={() => {
-          router.push('/');
-        }}
-      >
-        <ButtonText>Explore Tab Navigation</ButtonText>
-      </Button>
+  const handleFormSubmit = async () => {
 
-      <Button
-        size="md"
-        className="bg-primary-500 px-6 py-2 mt-5 rounded-full"
-        onPress={() => {
-          router.push('/test');
-        }}
-      >
-        <ButtonText>Not Found Test</ButtonText>
-      </Button>
-    
-    </Center>
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+
+    // Navigate to the form creation screen
+    //router.push('/forms/create');
+  }
+
+  return (
+    <ScrollView className="flex-1">
+
+      <Center className="">
+        <TouchableOpacity
+          className="p-4 bg-red-400 m-4 w-[250px] justify-center gap-2 flex-row items-center rounded-xl"
+          onPress={handleFormSubmit}
+        >
+          <PlusIcon size={16} color="#fff" />
+          <Text className="mr-2">   
+            Add New Form
+          </Text>
+        </TouchableOpacity>
+      </Center>
+      
+
+      {/* <FlatList
+        data={formData}
+        renderItem={renderFormItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      /> */}
+    </ScrollView>
   );
 }

23234954f91d1ee740f6bd09985dc423456a8d4f
Matthew <curran.wehttam@gmail.com>
2025-10-19 21:00:46 +1000

    Initial Commit of Form ID Specific Pages

---
+++ app/tabs/(tabs)/Forms/[id]/_layout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import { Drawer } from 'expo-router/drawer';
+import { DarkTheme, useNavigation } from '@react-navigation/native';
+import { Tabs } from 'expo-router';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { useColorScheme } from '@/components/useColorScheme';
+
+function DrawerIcon({ name, color }) {
+  return <FontAwesome size={18} style={{ marginRight: 8 }} name={name} color={color} />;
+}
+
+export default function DrawerLayout() {
+
+  const colorScheme = useColorScheme(); // 'light' or 'dark'
+  const isDark = colorScheme === 'dark';
+
+  return (
+    <Drawer
+      screenOptions={({ route }) => ({
+        headerShown:
+          route.name === 'Home' // Hide Header on Home Screen
+            ? useClientOnlyValue(false, false) 
+            : useClientOnlyValue(false, true),
+        drawerActiveTintColor: isDark ? '#3EB489' : '#059669',
+        drawerInactiveTintColor: isDark ? '#AAAAAA' : '#555555',
+        drawerLabelStyle: { fontSize: 20 },
+      })}
+    >
+    </Drawer>
+  );
+}
+

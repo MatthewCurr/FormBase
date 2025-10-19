bba72832f3d351d4b17e73d35f0d3bd8a0ada903
Matthew <curran.wehttam@gmail.com>
2025-10-19 21:33:50 +1000

    Initial Pages for Forms/

---
+++ app/tabs/(tabs)/Forms/[id]/Records.jsx
@@ -0,0 +1,14 @@
+
+// ================================
+// UI Component Imports
+// ================================
+import { Box } from '@/components/ui/box';
+
+export default function FormEdit() {
+
+  return (
+    <Box className="p-10 items-start">
+      <Text>Edit Page</Text>
+    </Box>
+  );
+}

fc5edcc3c33c3047f94f5a148e54355aff22270d
Matthew <curran.wehttam@gmail.com>
2025-10-19 19:10:01 +1000

    Removed Gradient.tsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Gradient from '@/assets/icons/Gradient';
 import { Box } from '@/components/ui/box';
 import { ScrollView } from 'react-native';
 import { Text } from '@/components/ui/text';
 import { Button, ButtonText } from '@/components/ui/button';
 import { useRouter } from 'expo-router';
-import { Icon } from '@/components/ui/icon';
 import { LinearGradient } from '@/components/custom/LinearGradient';
 import FileStack from '@/assets/icons/FileStack';
 import { useTheme } from '@react-navigation/native';

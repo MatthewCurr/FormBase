1cbf02111fe177e32810974f752c735f04fe207d
Matthew <curran.wehttam@gmail.com>
2025-10-22 16:07:41 +1000

    Layout for View

---
+++ app/tabs/(tabs)/Forms/[id]/view/_layout.jsx
@@ -0,0 +1,27 @@
+import { Tabs, useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import HapticButton from '@/components/custom/HapticButton'
+
+import { useTheme } from '@react-navigation/native';
+
+export default function EditTabsLayout() {
+  const router = useRouter();
+
+  const colours = useTheme().colors; 
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerLeft: () => (
+          <HapticButton
+            onPress={() => router.push('/tabs/Forms')} // navigate to forms
+            style={{ paddingHorizontal: 15 }}
+          >
+            <Ionicons name="arrow-back" size={24} color={colours.primary} />
+          </HapticButton>
+        ),
+        headerTitleAlign: 'center',
+      }}
+    />
+  );
+}

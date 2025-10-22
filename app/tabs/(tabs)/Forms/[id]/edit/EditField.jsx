3f4f800748ec0b220a95dce3cc5102075322dd4c
Matthew <curran.wehttam@gmail.com>
2025-10-22 18:33:41 +1000

    Added first field to field form.

--- app/tabs/(tabs)/Forms/[id]/edit/EditField.jsx
+++ app/tabs/(tabs)/Forms/[id]/edit/EditField.jsx
@@ -1,41 +1,21 @@
-import { useState, useEffect } from 'react';
-import { useRouter, useGlobalSearchParams } from 'expo-router';
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
 import { useTheme } from '@react-navigation/native';
 import FormBase from '@/components/custom/FormForm';
 import * as Haptics from 'expo-haptics';
-import { getForms, createForm, updateForm } from '@/restapi';
+import { createForm } from '@/restapi';
 
-export default function FormPage() {
-  const { id } = useGlobalSearchParams(); // If id exists → edit, else add
+export default function AddForm() {
   const router = useRouter();
 
   const [formData, setFormData] = useState({ name: '', description: '' });
-  const [loading, setLoading] = useState(true);
 
-  const isEditing = id && id !== 'add';
-
-  useEffect(() => {
-
-    if (isEditing) {
-      // Edit mode → fetch existing form
-      const fetchForm = async () => {
-        try {
-          const forms = await getForms();
-          const form = forms.find((f) => f.id.toString() === id);
-          if (form) setFormData({ name: form.name, description: form.description });
-        } catch (err) {
-          console.error(err);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchForm();
-    } else {
-      // Add mode → start with empty form
-      setFormData({ name: '', description: '' });
-      setLoading(false);
-    }
-  }, [id]);
+  const formFields = [
+    { name: 'name', label: 'Field Name', placeholder: 'Enter Field Name', required: true },
+  ];
+  
+  const titles = ["Add a Field"]
+  const buttons = ["Add Field"]
 
   const handleSubmit = async () => {
     if (!formData.name?.trim() || !formData.description?.trim()) {
@@ -44,11 +24,7 @@ export default function FormPage() {
     }
 
     try {
-      if (isEditing) {
-        await updateForm(formData, id);
-      } else {
-        await createForm(formData);
-      }
+      await createForm(formData);
       await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
       router.push('/tabs/Forms'); // go back to forms list
     } catch (err) {
@@ -57,15 +33,16 @@ export default function FormPage() {
     }
   };
 
-  if (loading) return null; // or a spinner
-
   return (
     <FormBase
       formData={formData}
       setFormData={setFormData}
       onSubmit={handleSubmit}
       onCancel={() => router.push('/tabs/Forms')}
-      isEditing={isEditing} // true if editing, false if adding
+      isEditing={false}
+      fields={formFields}
+      title={titles}
+      button={buttons}
     />
   );
 }

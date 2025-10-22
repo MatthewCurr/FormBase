e3280aa70e777f9c04bfe080ac95874397082607
Matthew <curran.wehttam@gmail.com>
2025-10-22 14:40:12 +1000

    Added Back Button to Tab Headers

--- app/tabs/(tabs)/Forms/[id]/_layout.jsx
+++ app/tabs/(tabs)/Forms/[id]/_layout.jsx
@@ -1,9 +1,29 @@
-import { Tabs } from 'expo-router';
+import { Tabs, useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import HapticButton from '@/components/custom/HapticButton'
+
+import { useTheme } from '@react-navigation/native';
 
 export default function EditTabsLayout() {
+  const router = useRouter();
+
+  const colours = useTheme().colors; 
+
   return (
-    <Tabs>
-      <Tabs.Screen name="Edit" options={{ title: 'Edit' }} />
+    <Tabs
+      screenOptions={{
+        headerLeft: () => (
+          <HapticButton
+            onPress={() => router.push('/tabs/Forms')} // navigate to forms
+            style={{ paddingHorizontal: 15 }}
+          >
+            <Ionicons name="arrow-back" size={24} color={colours.primary} />
+          </HapticButton>
+        ),
+        headerTitleAlign: 'center',
+      }}
+    >
+      <Tabs.Screen name="Add" options={{ title: 'Add' }} />
       <Tabs.Screen name="Map" options={{ title: 'Map' }} />
       <Tabs.Screen name="Records" options={{ title: 'Records' }} />
     </Tabs>

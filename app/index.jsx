2411a04fc8e803c622938177b4fd92e87cd88ab1
Matthew <curran.wehttam@gmail.com>
2025-10-25 19:30:33 +1000

    Removed FeatureCard

--- app/index.jsx
+++ app/index.jsx
@@ -10,21 +10,6 @@ import { useTheme } from '@react-navigation/native';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
-// const FeatureCard = ({ iconSvg: IconSvg, name, desc }) => {
-//   return (
-//     <Box
-//       className="flex-column md:flex-1 m-2 p-4 rounded-lg bg-background-0/40"
-//       key={name}
-//     >
-//       <Box className="items-center flex flex-row">
-//         <Icon as={IconSvg}/>
-//         <Text className="font-medium ml-2 text-xl">{name}</Text>
-//       </Box>
-//       <Text className="mt-2">{desc}</Text>
-//     </Box>
-//   );
-// };
-
 export default function Home() {
   const router = useRouter();
 

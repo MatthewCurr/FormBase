0437258094adfcfcf9956a10ae3b19d444633bc7
Matthew <curran.wehttam@gmail.com>
2025-10-25 20:00:47 +1000

    Added Record Title and optional Child components to Display.

--- components/custom/RecordDisplay.jsx
+++ components/custom/RecordDisplay.jsx
@@ -13,8 +13,9 @@ import RecordItem from '@/components/custom/RecordItem'
  * @param {Object} props
  * @param {Object} props.item - The form data (id and recorded fields).
  * @param {Object} props.colours - Theme colours for background/text.
+ * @param {ReactElement} children - Child Components to render in display. 
  */
-export default function RecordDisplay({ record, colours }) {
+export default function RecordDisplay({ record, colours, children }) {
 
   console.log(`Record is`, record)
 
@@ -23,7 +24,13 @@ export default function RecordDisplay({ record, colours }) {
       className="mx-2 mb-4 p-4 rounded-xl"
       style={{ backgroundColor: colours.card }}
     >
-
+      {/* Display Record ID as Title */}
+      <Text
+        className="text-lg font-semibold"
+        style={{ color: colours.text }}
+      >
+        Record ID #{record.id}
+      </Text>
       {/* Render record list */}
       <FlatList
         data={Object.entries(record.values || {}).map(([key, value]) => ({ key, value }))}
@@ -34,7 +41,12 @@ export default function RecordDisplay({ record, colours }) {
           />
         )}
         keyExtractor={(record) => `${record.key}-${record.value}`}
+        className="mt-2"
       />
+
+      {/* If Child Components given render below */}
+      {children}
+
     </Box>
   );
 }
